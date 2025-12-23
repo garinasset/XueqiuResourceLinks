61f3026820eea0c211a2cb3f5d29a8c3e7bb6a3d
garinasset <comu@qq.com>
2025-12-24 00:31:21 +0800

    资源：增加finviz支持

--- XueqiuResourceLinks.user.js
+++ XueqiuResourceLinks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            雪球 · 第三方资源扩展
 // @namespace       https://github.com/garinasset/XueqiuResourceLinks
-// @version         7.6.2
+// @version         8.0.0
 //
 // @description     在雪球股票详情页侧边栏，添加相应“个股”的“第三方资源”，例如上证 e 互动、深交所互动易、SEC: EDGAR、港交所披露易等，点击即可跳转到对应个股的第三方资源站点，便利研究，提升生产力。
 //
@@ -354,8 +354,16 @@
             exchange: stock.exchange,
             urlFetcher: async () => ({
                 text: 'Stocktwits',
-                url: `https://stocktwits.com/symbol/${stock.code}`,
+                url: `https://stocktwits.com/symbol/${encodeURIComponent(stock.code)}`,
                 favicon: 'https://stocktwits.com/favicon.ico'
+            })
+        });
+        thirdPartyResources.push({
+            exchange: stock.exchange,
+            urlFetcher: async () => ({
+                text: 'finviz',
+                url: `https://finviz.com/quote.ashx?t=${encodeURIComponent((stock.code).replace(/\./g, '-'))}`,
+                favicon: 'https://finviz.com/favicon.ico'
             })
         });
     }
